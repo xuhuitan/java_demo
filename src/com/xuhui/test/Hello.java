092ca863afff86130ba3d6b74ef3b22594addcf5
xuhuitan <490469186@qq.com>
2016-06-11 15:05:20 +0800

    eeee

--- src/com/xuhui/test/Hello.java
+++ src/com/xuhui/test/Hello.java
@@ -3,7 +3,7 @@ package com.xuhui.test;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("hello boy aagc!");
-		System.out.println("hello boy aagc552!");
+		System.out.println("hello boy2 aagc!");
+		System.out.println("hello boy2 aagc552!");
 	}
 }
