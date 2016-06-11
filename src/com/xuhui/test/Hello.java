4458bd39cf6eb6e9f8f9296ab41db7c329a3f81a
xuhuitan <490469186@qq.com>
2016-06-11 15:15:44 +0800

    Update Hello.java

--- src/com/xuhui/test/Hello.java
+++ src/com/xuhui/test/Hello.java
@@ -3,7 +3,7 @@ package com.xuhui.test;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("hello boy2 aagc!");
-		System.out.println("hello boy2 aagc5522!");
+		System.out.println("hello boy3 aagc!");
+		System.out.println("hello boy3 aagc5522!");
 	}
 }
