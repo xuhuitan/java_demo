4bd0186a00177173d539abae4486f0bd5ec40193
xuhuitan <490469186@qq.com>
2016-06-11 14:49:33 +0800

    Update Hello.java

--- src/com/xuhui/test/Hello.java
+++ src/com/xuhui/test/Hello.java
@@ -4,6 +4,6 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("hello boy aagc!");
-		System.out.println("hello boy aagc2!");
+		System.out.println("hello boy aagc552!");
 	}
 }
